c82cca2040a2d9a6fa8760a7da3cc2f3633fd322
znycheporuk <nycheporuk.zakhar@gmail.com>
2025-02-25 21:20:36 +0200

    rename zen

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -83,7 +83,7 @@ defaults write com.apple.dock wvous-bl-corner -int 0
 
 apps=(
   "/Applications/Telegram Desktop.app"
-  "/Applications/Zen Browser.app"
+  "/Applications/Zen.app"
   "/Applications/Zed.app"
   "/Applications/Ghostty.app"
   "/Applications/Slack.app"
